8f748762028b05f7ed06e507fee605f61657bcc4
Srishti Gupta <246302820+SrishtiGupta08@users.noreply.github.com>
2026-06-17 01:51:27 +0530

    Add files via upload

---
+++ Pattern-3.py
@@ -0,0 +1,28 @@
+"""
+*
+**
+***
+****
+*****
+
+"""
+
+n=int(input("enter the number:"))
+
+for i in range(1,n+1):
+
+    print("*"*i)
+
+
+
+ # now this pattern with while loop
+
+n=int(input("enter the number:"))
+
+i=1
+
+while(i<=n):
+
+    print("*"*i)
+    
+    i+=1
